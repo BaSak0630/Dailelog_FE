e93be13fd0a1e7d6e7f75ce4afd8a8844b190140
BaSak0630 <babamba125@gmail.com>
2024-07-03 00:26:37 +0900

    간단하게 기존 백엔드 api를 프론트에 보여지게 하기 위해 간단한 view와 components를 개발

--- src/views/WriteView.vue
+++ src/views/WriteView.vue
@@ -1,34 +1,40 @@
 <script setup lang="ts">
-import {ref } from 'vue'
-import axios from 'axios'
+import { ref } from "vue";
 
-const title = ref("")
-const content = ref("")
+import axios from "axios";
+import { useRouter } from "vue-router";
 
-const write = function() {
-  axios.post("http://localhost:8080/posts",{
-    title: title.value,
-    content: content.value
-  })
-}
+const title = ref("");
+const content = ref("");
 
+const router = useRouter();
+
+const write = function () {
+  axios
+    .post("/api/posts", {
+      title: title.value,
+      content: content.value,
+    })
+    .then(() => {
+      router.replace({ name: "home" });
+    });
+};
 </script>
 
 <template>
   <div>
-    <el-input v-model="title" placeholder="제목을 입력해주세요"/>
+    <el-input v-model="title" placeholder="제목을 입력해주세요" />
   </div>
 
   <div class="mt-2">
-    <div>
-      <el-input v-model="content" type="textarea" rows="15"/>
-    </div>
+    <el-input v-model="content" type="textarea" rows="15" />
+  </div>
 
-    <div class="mt-2">
-      <el-button type="primary" @click="write">글 작성완료</el-button>
+  <div class="mt-2">
+    <div class="d-flex justify-content-end">
+      <el-button type="primary" @click="write()">작성완료</el-button>
     </div>
   </div>
 </template>
-<style>
 
-</style>
+<style></style>
